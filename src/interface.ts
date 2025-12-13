d25a97efcf566f98a3ebe3da123eed2067472007
Jahid458 <Jahidul898@outlook.com>
2025-12-14 02:25:17 +0600

    literal types  interface

--- src/interface.ts
+++ src/interface.ts
@@ -15,7 +15,7 @@ function serverChai(order:ChaiOrder){
                 console.log(order);
 }
 
-type teaRecipe ={ 
+type teaRecipe = { 
         water: number;
         milk: number
 }
@@ -26,3 +26,61 @@ class MasalaChai implements teaRecipe {
         milk = 50 ;
 }
 
+interface CupSize  {
+   size:  'small' | 'large';
+} 
+
+
+class chai implements CupSize {
+        size: "small" | "large" = "large"
+}
+
+// when we use in class we decide to no needed type this needed to interface  
+
+// type respose = {ok: true} | {ok: false}
+
+// class myRes implements respose {
+//         ok: boolean = true;
+// }
+
+
+type TeaType = "masla" | 'ginger' | 'lemon' // thats called literal type 
+
+
+function orderChai(t: TeaType){
+        console.log(t);
+}
+
+
+type BaseChai = {teaLeaves:  number};
+type Extra = {masla: number}
+type MasalaaChai = BaseChai & Extra
+
+const cup : MasalaaChai = { 
+        teaLeaves : 2 ,
+        masla :1   
+}
+
+type User = { 
+        userName : string , 
+        bio?:string 
+}
+
+
+const u1: User = {userName: 'Jahid'} 
+const u2: User = {userName: 'Ananto', bio: 'abantor.ai'}
+
+
+
+type Config = {
+        readonly appName : string,
+        version : number
+}
+
+
+const cfg : Config ={
+        appName : 'MasterJi',
+        version: 1
+}
+
+// c1.appName = 'Mnsa'  --> Its its readonluy so when you set up one name it not contain naother 
